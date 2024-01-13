f3bd54ad3fca8a5804967557bd6e33b155916ffc
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-01-13 20:31:14 +0200

    Create 04_generating_zero_one_vectors.py

---
+++ 01_Recursion/01_lab/04_generating_zero_one_vectors.py
@@ -0,0 +1,14 @@
+def generating_zero_one_vectors(index, vector):
+    if index >= len(vector):
+        print(*vector, sep='')
+        return
+
+    for num in range(2):
+        vector[index] = num
+        generating_zero_one_vectors(index + 1, vector)
+
+
+n_vectors = int(input())
+vector = [0] * n_vectors
+
+generating_zero_one_vectors(0, vector)
