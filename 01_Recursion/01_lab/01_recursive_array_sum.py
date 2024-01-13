54b88e309d7792ffdf12460d05b3136dcfe890fe
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-01-13 18:49:27 +0200

    Update 01_recursive_array_sum.py

--- 01_Recursion/01_lab/01_recursive_array_sum.py
+++ 01_Recursion/01_lab/01_recursive_array_sum.py
@@ -1,8 +1,8 @@
 def sum_numbers(numbers, index):
-    if index == len(numbers) - 1:
+    if index == len(numbers) - 1:  # base case
         return numbers[index]
 
-    return numbers[index] + sum_numbers(numbers, index + 1)
+    return numbers[index] + sum_numbers(numbers, index + 1)  # recursive call
 
 
 nums = [int(x) for x in input().split()]
